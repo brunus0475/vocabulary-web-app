c6dce97744cdce37a6d7fd63124e73c11a12a7a6
brunus0475 <43059446+brunus0475@users.noreply.github.com>
2024-01-08 20:41:37 +0100

    Update AddTermPage.js

--- frontend/src/components/AddTermPage.js
+++ frontend/src/components/AddTermPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 function AddTermPage() {
   const [termData, setTermData] = useState({
