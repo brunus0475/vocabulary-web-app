5a037902214f1ed6f297c1a9390b912873f8e1af
brunus0475 <43059446+brunus0475@users.noreply.github.com>
2024-01-08 21:11:39 +0100

    Update Term.js

--- backend/models/Term.js
+++ backend/models/Term.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const TermSchema = new mongoose.Schema({
   term: { type: String, required: true },
   definition: { type: String, required: true },
-  category: { type: String, required: true }
+  category: { type: String, required: false }
 });
 
 const Term = mongoose.model("Term", TermSchema);
